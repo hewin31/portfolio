053af9fcfd61b13de901e8ded7e8ec23577b3ebc
hewin31 <zawaurdoo3121@gmail.com>
2025-12-16 09:55:39 +0530

    added button functions to external links

---
+++ script.js
@@ -0,0 +1,15 @@
+function externallink(name)
+{
+    if(name === 'linkedin')
+    {
+        window.open('https://www.linkedin.com/in/hewin-sheraj3108/', '_blank');
+    }
+    else if(name === 'github')
+    {
+        window.open('https://github.com/madeinhewin', '_blank');
+    }
+    else if(name === 'leetcode')
+    {
+        window.open('https://www.leetcode.com/u/hewinboi2005/','_blank');
+    }
+}
